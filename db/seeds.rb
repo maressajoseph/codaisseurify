7c91af6f7a9c7adeca5b6b0ada27449c95fe66ef
maressajoseph <maressa.joseph@gmail.com>
2017-05-26 19:29:12 +0200

    Change column type + update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+juan_luis_guerra = Artist.new(name: "Juan Luis Guerra")
+pablo_alboran = Artist.new(name: "Pablo Alborán")
+mana = Artist.new(name: "Maná")
+
+frio_frio = Song.new(name: "Frío, Frío", artist: juan_luis_guerra, album: "Areíto", release_date: "1992")
+muchatita_linda = Song.new(name: "Muchatita Linda", artist: juan_luis_guerra, album: "Todo Tiene Su Hora", release_date: "2014")
+perdoname = Song.new(name: "Perdóname", artist: pablo_alboran, album: "Pablo Alborán", release_date: "2011")
+pasos_de_cero = Song.new(name: "Pasos De Cero", artist: pablo_alboran, album: "Terral", release_date: "2014")
+mi_verdad = Song.new(name: "Mi Verdad", artist: mana, album: "Cama Incendiada", release_date: "2015")
