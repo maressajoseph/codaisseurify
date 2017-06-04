b62ee182d371bfe3a40001f079eea287183646f1
maressajoseph <maressa.joseph@gmail.com>
2017-06-04 23:19:00 +0200

    Styling order menu and navbar

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -1,5 +1,5 @@
 var addSong = document.getElementByClass("add-song");
 
 addSong.onclick = function(){
-  <%= render 'songs/new', artist: @artist, song: @artist.songs.new %>
-}
+  render :'songs/new'
+};
